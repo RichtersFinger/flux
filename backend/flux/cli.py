8dd6939b2af7eb81104be5e320363d8f43f0a232
RichtersFinger <steffen.finger.privat@gmail.com>
2025-08-03 21:34:28 +0200

    [be, cli] re-structure command tree for indexing

--- backend/flux/cli.py
+++ backend/flux/cli.py
@@ -1,12 +1,11 @@
 """`flux`-CLI definition."""
 
 from typing import Optional
-import sys
 from pathlib import Path
 from importlib.metadata import version
 
 import filetype
-from befehl import Parser, Option, Cli, Command
+from befehl import Parser, Option, Cli, Command, Argument
 
 
 def parse_as_flux_dir(data) -> tuple[bool, Optional[str], Optional[Path]]:
@@ -20,41 +19,125 @@ def parse_as_flux_dir(data) -> tuple[bool, Optional[str], Optional[Path]]:
     return True, None, path
 
 
-class Index(Command):
-    """Subcommand for building index."""
+index_location = Option(
+    ("-i", "--index-location"),
+    helptext="index/working directory (default uses current)",
+    nargs=1,
+    parser=parse_as_flux_dir,
+)
+dry_run = Option(
+    "--dry-run",
+    helptext="test effects before committing",
+    parser=Parser.parse_as_bool,
+)
+batch = Option(
+    "--batch",
+    helptext=(
+        "process immediate children in given targets as if they were "
+        + "passed individually"
+    ),
+)
+verbose = Option(("-v", "--verbose"), helptext="verbose output")
 
-    input_ = Option(
-        ("-i", "--input"),
-        helptext="target directory that should be indexed",
-        nargs=1,
-        parser=Parser.parse_as_dir,
-    )
-    output = Option(
-        ("-o", "--output"),
-        helptext="working directory (default uses current)",
-        nargs=1,
-        parser=parse_as_flux_dir,
-    )
-    verbose = Option(("-v", "--verbose"), helptext="verbose output")
 
-    def validate(self, args):
-        if self.input_ not in args:
-            return False, "Missing input. Call with option '-h' for help."
-        return True, None
+class CreateIndex(Command):
+    """Subcommand for creating index."""
+
+    index_location = index_location
+    verbose = verbose
 
     def run(self, args):
-        input_: Path = args[self.input_][0]
-        for file in input_.glob("**/*"):
+        pass
+
+
+class AddToIndex(Command):
+    """Subcommand for adding to index."""
+
+    index_location = index_location
+    batch = batch
+    auto = Option(
+        "--auto",
+        helptext="heuristically detect content type",
+    )
+    type_ = Option(
+        "--type",
+        helptext=(
+            "explicitly specify content type (one of 'movie', 'series', "
+            + "'collection')"
+        ),
+        nargs=1,
+        parser=Parser.parse_with_values(["movie", "series", "collection"]),
+    )
+    dry_run = dry_run
+    verbose = verbose
+
+    target = Argument(
+        "target",
+        helptext="target directory/file that should be added to the index",
+        nargs=-1,
+        parser=Parser.parse_as_path,
+    )
+
+    def validate(self, args):
+        if self.type_ not in args and self.auto not in args:
+            return (
+                False,
+                "Either '--auto' or '--type' is required for this operation.",
+            )
+        return True, ""
+
+    def run(self, args):
+        target: Path = args[self.target][0]
+        for file in target.glob("**/*"):
             if file.is_file():
                 ft = filetype.guess(file)
                 print(file, " -> ", "-" if ft is None else ft.mime)
+
+
+class RmFromIndex(Command):
+    """Subcommand for removing from index."""
+
+    dry_run = dry_run
+    index_location = index_location
+    batch = batch
+    verbose = verbose
+
+    target = Argument(
+        "target",
+        helptext="target directory/file that should be removed from index",
+        nargs=-1,
+        parser=Parser.parse_as_path,
+    )
+
+    def run(self, args):
+        pass
+
+
+class Index(Command):
+    """Subcommand for indexing."""
+
+    create = CreateIndex("create", helptext="create a new index")
+    add = AddToIndex("add", helptext="add resources to an exiting index")
+    rm = RmFromIndex("rm", helptext="delete resources from an exiting index")
+
+    def run(self, args):
+        self._print_help()
+
+
+class Run(Command):
+
+    index_location = index_location
+    verbose = verbose
+
+    def run(self, args):
+        print("Not implemented yet.")
 
 
 class FluxCli(Cli):
     """CLI for `flux`."""
 
     index = Index("index", helptext="create index for given source directory")
-
+    run_ = Run("run", helptext="run flux app")
     version = Option(("-v", "--version"), helptext="prints library version")
 
     def run(self, args):
