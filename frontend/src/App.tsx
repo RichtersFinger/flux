6a7f67aad3236bb1f3b1e9e2ce0c7a615a54cf78
RichtersFinger <steffen.finger.privat@gmail.com>
2025-08-16 13:14:36 +0200

    [fe] create initial mock for app-layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,29 @@ import Logo from "./components/Logo";
 
 export default function App() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center bg-gray-800">
-      <Logo className="text-gray-300" src="/favicon.svg" text="flux" />
+    <div className="h-screen w-screen bg-gray-800 overflow-x-clip overflow-y-auto">
+      {/* toolbar */}
+      <div className="fixed w-full h-toolbar flex flex-row justify-between items-center bg-gray-900 shadow-xl">
+        <div className="mx-2">
+          <Logo className="text-gray-300" src="/favicon.svg" text="flux" />
+        </div>
+        <div>
+          <input
+            type="text"
+            className="bg-gray-100 text-gray-900 rounded px-2 py-1"
+            placeholder="Search ..."
+          />
+        </div>
+        <div className="mx-2">
+          <div className="bg-gray-100 border-gray-400 border-2 rounded-full w-12 aspect-square" />
+        </div>
+      </div>
+      {/* content */}
+      <div className="mt-toolbar">
+        <div className="h-full flex flex-col text-gray-100 items-center">
+          <span className="p-4">tbd</span>
+        </div>
+      </div>
     </div>
   );
 }
