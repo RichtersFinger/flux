7a81fadf7c394f8c77fa6cdbb8fba8d342f84cab
RichtersFinger <steffen.finger.privat@gmail.com>
2025-11-26 17:58:49 +0100

    [fe, components] add basic TextInput

---
+++ frontend/src/components/base/TextInput.tsx
@@ -0,0 +1,39 @@
+interface TextInputProps {
+  className?: string;
+  value?: string;
+  type?: "text" | "password";
+  placeholder?: string;
+  icon?: React.ReactNode;
+  maxLength?: number;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function TextInput({
+  className = "",
+  value,
+  type = "text",
+  placeholder,
+  icon,
+  maxLength,
+  onChange,
+}: TextInputProps) {
+  return (
+    <div className="relative">
+      {icon ? (
+        <div className="absolute h-full pointer-events-none bg-slate-400 rounded-l noselect">
+          <div className="flex flex-row h-full px-2 items-center">{icon}</div>
+        </div>
+      ) : null}
+      <input
+        className={`p-1 px-2 border border-gray-300 rounded outline-none focus:outline-gray-200 outline-1 ${className} ${
+          icon ? "indent-7" : ""
+        }`}
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        maxLength={maxLength}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
