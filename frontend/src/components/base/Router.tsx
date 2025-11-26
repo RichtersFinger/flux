a860ab82636f5ef5c61b867b4e49b81517788711
RichtersFinger <steffen.finger.privat@gmail.com>
2025-11-26 20:28:07 +0100

    [fe, components] add basic router-component

---
+++ frontend/src/components/base/Router.tsx
@@ -0,0 +1,87 @@
+import { useSyncExternalStore } from "react";
+
+// types
+interface LocationStore {
+  pathname: string;
+  search?: URLSearchParams;
+  subscribers: Set<() => void>;
+  getPathname: () => string;
+  getSearch: () => URLSearchParams | undefined;
+  subscribe: (callback: () => void) => () => void;
+  navigate: (pathname: string, search?: string, useHistory?: boolean) => void;
+}
+
+// custom store implementation
+const locationStore: LocationStore = {
+  pathname: window.location.pathname,
+  search: new URLSearchParams(window.location.search),
+  subscribers: new Set(),
+  getPathname() {
+    return locationStore.pathname;
+  },
+  getSearch() {
+    return locationStore.search;
+  },
+  subscribe(callback) {
+    locationStore.subscribers.add(callback);
+    return () => locationStore.subscribers.delete(callback);
+  },
+  navigate(pathname, search, useHistory = true) {
+    // update store
+    locationStore.pathname = pathname;
+    if (search !== undefined) {
+      if (typeof search === "string")
+        locationStore.search = new URLSearchParams(search);
+      else locationStore.search = search;
+    }
+
+    // update location
+    if (useHistory) {
+      const query = locationStore.search?.toString() ?? "";
+      window.history.pushState(
+        {},
+        "flux" + locationStore.pathname,
+        locationStore.pathname + (query === "" ? "" : `?${query}`)
+      );
+    }
+
+    // notify subscribers
+    locationStore.subscribers.forEach((callback) => callback());
+  },
+};
+
+// handle onpopstate-events
+window.addEventListener("popstate", () =>
+  locationStore.navigate(
+    window.location.pathname,
+    window.location.search,
+    false
+  )
+);
+
+/**
+ * Router-hook that provides navigation
+ * @prop "navigate" navigate to new location
+ */
+export function useRouter() {
+  return {
+    navigate: locationStore.navigate,
+  };
+}
+
+/**
+ * Location-hook that returns current location.
+ * @prop "pathname" current path
+ * @prop "search" current url query
+ */
+export function useLocation() {
+  const pathname = useSyncExternalStore(
+    locationStore.subscribe,
+    locationStore.getPathname
+  );
+  const search = useSyncExternalStore(
+    locationStore.subscribe,
+    locationStore.getSearch
+  );
+  return { pathname, search };
+}
