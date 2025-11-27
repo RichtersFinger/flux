d53e4700ee2bba8ab7fe97feaf4ee1601ec60381
RichtersFinger <steffen.finger.privat@gmail.com>
2025-11-27 13:50:00 +0100

    [fe, components] add css-variables for PlaceholderBox-animation

--- frontend/src/components/base/PlaceholderBox.tsx
+++ frontend/src/components/base/PlaceholderBox.tsx
@@ -1,3 +1,5 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
 interface PlaceholderBoxProps {
   className?: string;
   onClick?: (event: React.MouseEvent) => void;
@@ -7,13 +9,33 @@ export default function PlaceholderBox({
   className = "",
   onClick,
 }: PlaceholderBoxProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [width, setWidth] = useState(1000);
+
+  useLayoutEffect(() => {
+    function handleResize() {
+      if (!ref.current) return;
+      setWidth(ref.current.offsetWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div
+      ref={ref}
       className={`bg-gray-900 border-gray-700 border-2 border-opacity-0 ${className}`}
       onClick={onClick}
     >
       <div className="overflow-clip relative h-full w-full">
-        <div className="absolute bg-white -top-10 blur-lg placeholder-box-bar" />
+        <div
+          className="absolute bg-white -top-10 blur-lg placeholder-box-bar"
+          style={
+            {
+              "--range": `${2.5 * width}px`,
+            } as React.CSSProperties
+          }
+        />
       </div>
     </div>
   );
