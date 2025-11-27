3d1b6e797c28cc5f461d7f78d7ff449827878220
RichtersFinger <steffen.finger.privat@gmail.com>
2025-11-27 16:30:50 +0100

    [fe, app] add basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
+import { useLocation } from "./components/base/Router";
 import Page from "./components/flux/Page";
 import Browse from "./components/flux/pages/browse/Browse";
+import LoginRegister from "./components/flux/pages/login_register/LoginRegister";
 
 export default function App() {
+  const location = useLocation();
+
   return (
-    <div className="h-screen w-screen bg-gray-800 overflow-x-clip overflow-y-auto">
+    <div className="h-screen w-screen bg-gray-800 overflow-x-clip overflow-y-hidden">
       <Page>
-        <Browse />
+        {{ "/browse": <Browse />, "/login": <LoginRegister /> }[location.pathname]}
       </Page>
     </div>
   );
