b7d39d9f190173f83c1fc980130870c524fb7f73
RichtersFinger <steffen.finger.privat@gmail.com>
2025-11-28 22:17:39 +0100

    [be, api] add cookie-auth decorator

--- backend/flux/api/common.py
+++ backend/flux/api/common.py
@@ -5,6 +5,9 @@ from functools import wraps
 
 from flask import request, Response
 
+from flux.db import Transaction
+from flux.config import FluxConfig
+
 
 def header_auth(password: Optional[str]):
     """Protect endpoint with auth via 'X-Flux-Auth'-header."""
@@ -15,6 +18,42 @@ def header_auth(password: Optional[str]):
             if request.headers.get("X-Flux-Auth") != password:
                 return Response("FAILED", mimetype="text/plain", status=401)
             return route()
+
+        return __
+
+    return decorator
+
+
+def session_cookie_auth(delete_cookie_on_fail: bool = False):
+    """
+    Protect endpoint with auth via session-cookie.
+
+    If credentials are valid, decorated view functions are called with
+    session-id and username passed as positional arguments.
+    """
+
+    def decorator(route):
+        @wraps(route)
+        def __():
+            if FluxConfig.SESSION_COOKIE_NAME not in request.cookies:
+                return Response(
+                    "UNAUTHORIZED", mimetype="text/plain", status=401
+                )
+            session_id = request.cookies[FluxConfig.SESSION_COOKIE_NAME]
+            with Transaction(
+                FluxConfig.INDEX_LOCATION / FluxConfig.INDEX_DB_FILE,
+                readonly=True,
+            ) as t:
+                t.cursor.execute(
+                    "SELECT username FROM sessions WHERE id=?", (session_id,)
+                )
+            if len(t.data) == 0:
+                # unknown session
+                r = Response("UNAUTHORIZED", mimetype="text/plain", status=401)
+                if delete_cookie_on_fail:
+                    r.delete_cookie(FluxConfig.SESSION_COOKIE_NAME)
+                return r
+            return route(session_id, t.data[0][0])
 
         return __
 
