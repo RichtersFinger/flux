3074ba44c39482df887c739fe8ccdc4f400367b1
RichtersFinger <steffen.finger.privat@gmail.com>
2025-11-28 22:46:55 +0100

    [fe, app] add constants for api-requests
    and make simple request to fetch login-status

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,47 @@
+import { useEffect, useState } from "react";
+
 import { useLocation } from "./components/base/Router";
 import Page from "./components/flux/Page";
 import Browse from "./components/flux/pages/browse/Browse";
 import LoginRegister from "./components/flux/pages/login_register/LoginRegister";
 
+export const BASE_URL = process.env.REACT_APP_API_BASE_URL ?? "";
+export const DEFAULT_FETCH_OPTIONS: RequestInit = {
+  credentials: process.env.REACT_APP_API_BASE_URL ? "include" : "same-origin",
+};
+
 export default function App() {
   const location = useLocation();
+
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  // check for valid session
+  useEffect(() => {
+    fetch(`${BASE_URL}/api/v0/user/session`, { ...DEFAULT_FETCH_OPTIONS })
+      .then((response) => {
+        if (response.status === 200) setLoggedIn(true);
+        else setLoggedIn(false);
+      })
+      .catch((error) => {
+        setLoggedIn(false);
+        console.error(error);
+      });
+  }, [setLoggedIn]);
 
   return (
     <div className="h-screen w-screen bg-gray-800 overflow-x-clip overflow-y-hidden">
       <Page>
-        {{ "/browse": <Browse />, "/login": <LoginRegister /> }[location.pathname]}
+        {
+          { "/browse": <Browse />, "/login": <LoginRegister /> }[
+            location.pathname
+          ]
+        }
       </Page>
+      <div
+        className={`w-3 aspect-square rounded-full ${
+          loggedIn ? "bg-green-500" : "bg-red-500"
+        }`}
+      />
     </div>
   );
 }
