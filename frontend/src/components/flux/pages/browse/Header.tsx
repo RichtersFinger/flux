4d0da1f696351fbb9ab53837de291139d7399e4f
RichtersFinger <steffen.finger.privat@gmail.com>
2025-12-02 23:41:39 +0100

    [fe, pages] fix header z-position on Browse-page

--- frontend/src/components/flux/pages/browse/Header.tsx
+++ frontend/src/components/flux/pages/browse/Header.tsx
@@ -2,7 +2,7 @@ import Logo from "../../../base/Logo";
 
 export default function Header() {
   return (
-    <div className="fixed w-full h-toolbar flex flex-row justify-between items-center bg-gradient-to-r from-gray-800 via-gray-900 to-gray-900 shadow-2xl border-b border-gray-900">
+    <div className="fixed w-full h-toolbar flex flex-row justify-between items-center z-50 bg-linear-to-r from-gray-800 via-gray-900 to-gray-900 shadow-2xl border-b border-gray-900">
       {/* logo */}
       <div className="mx-2">
         <Logo className="text-gray-100" src="/favicon.svg" text="flux" />
