829e82b395cca9e76a3e769e3bb2dfde6286a453
RichtersFinger <steffen.finger.privat@gmail.com>
2025-12-02 17:52:47 +0100

    [fe, types, store] add initial SessionStore

---
+++ frontend/src/store.ts
@@ -0,0 +1,34 @@
+import { createShallowStore } from "./components/base/Store";
+import type { APIResponse, SessionStore } from "./types";
+import { pFetch } from "./util/api";
+
+export const useSessionStore = createShallowStore<SessionStore>((get, set) => ({
+  loggedIn: false,
+  checkLogin: () => {
+    pFetch("/api/v0/user/session")
+      .then((response) => {
+        if (!response.ok) {
+          response.text().then((text) => console.error(text));
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then((json: APIResponse) => {
+        if (json.meta.ok) get().setLoggedIn(true);
+        else get().setLoggedIn(false);
+      })
+      .catch((error) => {
+        get().setLoggedIn(false);
+        console.error(error);
+      });
+  },
+  setLoggedIn: (loggedIn) => {
+    set({ loggedIn });
+  },
+  userConfiguration: {},
+  setUserConfiguration: (userConfiguration) => {
+    set((state) => ({
+      userConfiguration: { ...state.userConfiguration, ...userConfiguration },
+    }));
+  },
+}));
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,13 +1,26 @@
 export interface APIResponseMD {
-    ok: boolean;
-    error?: {
-        code: number;
-        short: string;
-        long: string;
-    }
+  ok: boolean;
+  error?: {
+    code: number;
+    short: string;
+    long: string;
+  };
 }
 
-export interface APIResponse<ContentType = any> {
-    meta: APIResponseMD;
-    content?: ContentType;
+export interface APIResponse<ContentType = unknown> {
+  meta: APIResponseMD;
+  content?: ContentType;
+}
+
+interface UserConfiguration {
+  volume?: number;
+  autoplay?: boolean;
+}
+
+export interface SessionStore {
+  loggedIn: boolean;
+  checkLogin: () => void;
+  setLoggedIn: (loggedIn: boolean) => void;
+  userConfiguration: UserConfiguration;
+  setUserConfiguration: (userConfiguration: Partial<UserConfiguration>) => void;
 }
