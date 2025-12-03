0ed946f76a96698408afaa99502d39c7879f9eee
RichtersFinger <steffen.finger.privat@gmail.com>
2025-12-03 17:38:24 +0100

    [fe, components] add ContextMenu-component

---
+++ frontend/src/components/base/ContextMenu.tsx
@@ -0,0 +1,69 @@
+type MenuPosition = "bl" | "br" | "tl" | "tr";
+
+const PositionStyles: Record<MenuPosition, string> = {
+  bl: "right-0",
+  br: "left-0",
+  tl: "right-0 -translate-y-full -top-2",
+  tr: "left-0 -translate-y-full -top-2",
+};
+
+interface ContextMenuItem {
+  id: string;
+  content: string | React.ReactNode;
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+interface ContextMenuProps {
+  className?: string;
+  open: boolean;
+  position?: MenuPosition;
+  title?: string | React.ReactNode;
+  items?: ContextMenuItem[];
+  children?: React.ReactNode;
+  onDismiss?: () => void;
+}
+
+export default function ContextMenu({
+  className = "",
+  open,
+  position = "bl",
+  title,
+  items,
+  children,
+  onDismiss,
+}: ContextMenuProps) {
+  return (
+    <div className="relative overflow-visible">
+      {children}
+      {
+        /* cover entire screen to allow dismissing anywhere outside menu */
+        open && onDismiss !== undefined && (
+          <div
+            className="fixed top-0 left-0 h-screen w-screen"
+            onClick={onDismiss}
+          />
+        )
+      }
+      {open && (items?.length ?? 0 > 0) && (
+        <div
+          className={`w-full pointed-div absolute rounded bg-gray-800 border-gray-400 text-gray-200 border-2 text-nowrap select-none flex flex-col m-1 ${PositionStyles[position]} ${className}`}
+        >
+          {title ? (
+            <div className="px-3 py-2 mb-0 bg-gray-400 text-gray-900">
+              {title}
+            </div>
+          ) : null}
+          {items?.map((item) => (
+            <div
+              key={item.id}
+              className="w-full truncate px-3 py-2 hover:cursor-pointer hover:bg-gray-400 hover:text-gray-800"
+            >
+              {item.content}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
--- frontend/src/components/flux/pages/browse/Header.tsx
+++ frontend/src/components/flux/pages/browse/Header.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+import { FiUser, FiSettings, FiLogOut } from "react-icons/fi";
+
 import { useSessionStore } from "../../../../store";
 import Avatar from "../../../base/Avatar";
+import ContextMenu from "../../../base/ContextMenu";
 import Logo from "../../../base/Logo";
 
 export default function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
+
   const { userConfiguration } = useSessionStore();
   return (
     <div className="fixed px-1 w-full h-toolbar flex flex-row justify-between items-center z-50 bg-linear-to-r from-gray-800 via-gray-900 to-gray-900 shadow-2xl border-b border-gray-900">
@@ -19,7 +25,42 @@ export default function Header() {
         />
       </div>
       {/* user */}
-      <Avatar username={userConfiguration.user?.name} />
+      <ContextMenu
+        className="min-w-48"
+        open={openMenu}
+        title={
+          <div className="flex flex-row space-x-2 items-center">
+            <FiUser size={20} />
+            <span>{userConfiguration.user?.name}</span>
+          </div>
+        }
+        items={[
+          {
+            id: "settings",
+            content: (
+              <div className="flex flex-row space-x-2 items-center">
+                <FiSettings size={20} />
+                <span>Settings</span>
+              </div>
+            ),
+          },
+          {
+            id: "logout",
+            content: (
+              <div className="flex flex-row space-x-2 items-center">
+                <FiLogOut size={20} />
+                <span>Logout</span>
+              </div>
+            ),
+          },
+        ]}
+        onDismiss={() => setOpenMenu(false)}
+      >
+        <Avatar
+          username={userConfiguration.user?.name}
+          onClick={() => setOpenMenu((state) => !state)}
+        />
+      </ContextMenu>
     </div>
   );
 }
