96dd49058c705aa7860849d163ef16c39ff8cc85
RichtersFinger <steffen.finger.privat@gmail.com>
2025-12-13 17:27:39 +0100

    [fe, pages, browse] implement 'load-more' option to Content

--- frontend/src/components/flux/pages/browse/Content.tsx
+++ frontend/src/components/flux/pages/browse/Content.tsx
@@ -1,8 +1,13 @@
 import { useEffect, useState } from "react";
+import { FiChevronDown } from "react-icons/fi";
+
 import type { APIResponse, Records } from "../../../../types";
-import RecordDisplay from "./RecordDisplay";
 import { formatAPIErrorMessage, pFetch } from "../../../../util/api";
 import { useToaster } from "../../../base/Toaster";
+import Spinner from "../../../base/Spinner";
+import RecordDisplay from "./RecordDisplay";
+
+const RANGE_INCREMENT = 5;
 
 interface ContentProps {
   title: string;
@@ -13,6 +18,8 @@ interface ContentProps {
 export default function Content({ title, url, params }: ContentProps) {
   const { toast } = useToaster();
 
+  const [range, setRange] = useState(RANGE_INCREMENT);
+  const [loading, setLoading] = useState(false);
   const [records, setRecords] = useState<Records | undefined>(undefined);
 
   useEffect(() => {
@@ -25,7 +32,7 @@ export default function Content({ title, url, params }: ContentProps) {
         "?" +
         new URLSearchParams({
           ...Object.fromEntries(params ?? []),
-          range: "0-5",
+          range: `${records?.records.length ?? 0}-${range}`,
         }).toString()
     )
       .then((response) => {
@@ -36,11 +43,24 @@ export default function Content({ title, url, params }: ContentProps) {
         return response.json();
       })
       .then((json: APIResponse<Records>) => {
+        setLoading(false);
         if (discard) return;
-        if (json.meta.ok && json.content) setRecords(json.content);
+        if (json.meta.ok && json.content)
+          setRecords(
+            (state) =>
+              ({
+                ...state,
+                ...json.content,
+                records: [
+                  ...(state?.records ?? []),
+                  ...(json.content?.records ?? []),
+                ],
+              } as Records)
+          );
         else toast(formatAPIErrorMessage(json.meta));
       })
       .catch((error) => {
+        setLoading(false);
         toast(
           formatAPIErrorMessage({
             error: { code: 0, short: "Connection error", long: error.message },
@@ -51,12 +71,13 @@ export default function Content({ title, url, params }: ContentProps) {
     return () => {
       discard = true;
     };
-  }, [url, params, setRecords, toast]);
+    // eslint-disable-next-line
+  }, [url, params, range, setRecords, setLoading, toast]);
 
   if (records?.records?.length === 0) return null;
 
   return (
-    <div className="select-none">
+    <div className="select-none w-full">
       <h5 className="text-3xl font-bold text-gray-100">{title}</h5>
       <div className="grid grid-flow-row sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 p-2">
         {records ? (
@@ -67,6 +88,30 @@ export default function Content({ title, url, params }: ContentProps) {
           <RecordDisplay />
         )}
       </div>
+      {records && records?.count > range ? (
+        <div className="relative flex items-center justify-center">
+          <div className="border-t border-2 w-32 border-gray-600" />
+          <div className="shrink mx-4 flex flex-row text-gray-500 space-x-2 items-center">
+            <span>Show more</span>
+            {loading ? (
+              <div className="p-1 border-2 rounded-full transition-colors bg-gray-900 border-gray-700 text-gray-700">
+                <Spinner size="xs" />
+              </div>
+            ) : (
+              <div
+                className="p-1 border-2 rounded-full transition-colors bg-gray-900 hover:bg-gray-700 border-gray-700 hover:border-gray-500 text-gray-700 hover:text-gray-500 hover:cursor-pointer"
+                onClick={() => {
+                  setLoading(true);
+                  setRange((state) => state + RANGE_INCREMENT);
+                }}
+              >
+                <FiChevronDown size={20} />
+              </div>
+            )}
+          </div>
+          <div className="border-t border-2 w-32 border-gray-600" />
+        </div>
+      ) : null}
     </div>
   );
 }
