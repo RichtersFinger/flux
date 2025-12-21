26c8efecbf3e5226969190234d14d1146b5acccf
RichtersFinger <steffen.finger.privat@gmail.com>
2025-12-21 17:46:26 +0100

    [be, api, static] adapt video-stream endpoint to latest changes

--- backend/flux/api/static.py
+++ backend/flux/api/static.py
@@ -1,13 +1,14 @@
 """Definition of the flux-api"""
 
 import re
+from pathlib import Path
 
 from flask import Flask, Response, request, send_from_directory
 
 from flux.config import FluxConfig
 from flux.db import Transaction
 from flux import exceptions
-from .common import header_auth, session_cookie_auth
+from .common import session_cookie_auth
 
 
 def register_api(app: Flask):
@@ -40,20 +41,44 @@ def register_api(app: Flask):
             FluxConfig.INDEX_LOCATION / FluxConfig.THUMBNAILS, t.data[0][0]
         )
 
-    @app.route("/video", methods=["GET"])
-    @header_auth(FluxConfig.PASSWORD)
-    def video():
-        headers = request.headers
-        if "range" not in headers:
+    # local cache for association between trackId and filepath
+    track_cache: dict[str, Path] = {}
+
+    @app.route("/video/<track_id>", methods=["GET"])
+    @session_cookie_auth()
+    def video(
+        # pylint: disable=unused-argument
+        *args,
+        track_id: str,
+    ):
+        if "range" not in request.headers:
             return Response(status=400)
 
-        video_path = FluxConfig.STATIC_PATH / request.args.get("id")
-        print(video_path.resolve())
+        video_path = track_cache.get(track_id)
+        if video_path is None:
+            with Transaction(
+                FluxConfig.INDEX_LOCATION / FluxConfig.INDEX_DB_FILE,
+                readonly=True
+            ) as t:
+                t.cursor.execute(
+                    "SELECT path FROM tracks WHERE id=?",
+                    (track_id,),
+                )
+            if len(t.data) > 0 and t.data[0][0] is not None:
+                track_cache[track_id] = Path(t.data[0][0])
+                video_path = track_cache[track_id]
+
+        if video_path is None:
+            raise exceptions.NotFoundException(f"Unknown track '{track_id}'.")
+
         size = video_path.stat().st_size
 
         chunk_size = 10**6  # ~1MB
-        print(headers["range"], int(re.sub(r"\D", "", headers["range"])))
-        start = int(re.sub(r"\D", "", headers["range"]))
+        # print(
+        #     request.headers["range"],
+        #     int(re.sub(r"\D", "", request.headers["range"])),
+        # )
+        start = int(re.sub(r"\D", "", request.headers["range"]))
         end = min(start + chunk_size, size - 1)
 
         content_lenght = end - start + 1
