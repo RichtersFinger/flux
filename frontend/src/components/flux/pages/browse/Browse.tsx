34aa75866d9271dfdd605c5e0be6870e3a3e0e8e
RichtersFinger <steffen.finger.privat@gmail.com>
2025-12-28 16:25:36 +0100

    [fe, pages, browse] fix 'Continue watching...'-block

--- frontend/src/components/flux/pages/browse/Browse.tsx
+++ frontend/src/components/flux/pages/browse/Browse.tsx
@@ -9,7 +9,11 @@ export default function Browse() {
     <>
       <Header />
       <div className="mt-toolbar h-[calc(100%-var(--spacing-toolbar))] flex flex-col px-3 py-6 space-y-4 overflow-y-auto show-dark-scrollbar">
-        <Content title="Continue watching ..." url="/api/v0/index/records" />
+        <Content
+          title="Continue watching ..."
+          url="/api/v0/index/records"
+          params={new URLSearchParams({ continue: "true" })}
+        />
         <Content
           title="Series"
           url="/api/v0/index/records"
