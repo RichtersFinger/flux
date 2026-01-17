f49744591b582dea754ed24c94e21e04e38879e7
RichtersFinger <steffen.finger.privat@gmail.com>
2026-01-17 17:31:03 +0100

    [fe, components] adjust Avatar styling
    hover events only if click-able

--- frontend/src/components/base/Avatar.tsx
+++ frontend/src/components/base/Avatar.tsx
@@ -11,7 +11,7 @@ export default function Avatar({
 }: AvatarProps) {
   return (
     <div
-      className={`aspect-square w-12 bg-gray-800 border-gray-500 border-2 text-gray-200 text-3xl rounded-full select-none hover:text-white hover:border-blue-400 hover:shadow-xs hover:shadow-blue-300 transition-colors hover:cursor-pointer ${className}`}
+      className={`aspect-square w-12 bg-gray-800 border-gray-500 border-2 text-gray-200 text-3xl rounded-full select-none ${onClick ? "hover:text-white hover:border-blue-400 hover:shadow-xs hover:shadow-blue-300 transition-colors hover:cursor-pointer": ""} ${className}`}
       onClick={onClick}
     >
       <div className="w-full h-full flex flex-row items-center justify-center font-semibold -translate-y-0.25">
