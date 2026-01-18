9b27b55c6b0a82c8eab1247cb6d7100988dcd943
RichtersFinger <steffen.finger.privat@gmail.com>
2026-01-18 10:38:40 +0100

    [fe, components, badge] fix styling for missing dismiss

--- frontend/src/components/base/Badge.tsx
+++ frontend/src/components/base/Badge.tsx
@@ -15,13 +15,15 @@ export default function Badge({
 }: BadgeProps) {
   return (
     <div
-      className={`flex flex-row space-x-2 items-center pl-3 pr-2 py-0.5 select-none bg-blue-500 text-gray-200 rounded-xl text-nowrap ${onClick ? "hover:cursor-pointer hover:bg-blue-400" : ""} ${className}`}
+      className={`flex flex-row space-x-2 items-center pl-3 ${onDismiss ? "pr-2" : "pr-3"} py-0.5 select-none bg-blue-500 text-gray-200 rounded-xl text-nowrap ${onClick ? "hover:cursor-pointer hover:bg-blue-400" : ""} ${className}`}
       onClick={onClick}
     >
       <div>{children}</div>
-      <div className="hover:cursor-pointer hover:text-white rounded-full hover:bg-blue-400 p-1 transition-colors">
-        <FiX size={16} onClick={onDismiss} />
-      </div>
+      {onDismiss ? (
+        <div className="hover:cursor-pointer hover:text-white rounded-full hover:bg-blue-400 p-1 transition-colors">
+          <FiX size={16} onClick={onDismiss} />
+        </div>
+      ) : null}
     </div>
   );
 }
