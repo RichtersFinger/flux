c18655fc52a9ed684605d88ab5b4d785e8d0c9e6
RichtersFinger <steffen.finger.privat@gmail.com>
2026-01-24 14:03:31 +0100

    [fe, components, modal] improve dismissing modal UX
    use onMouseDown instead of onClick

--- frontend/src/components/base/BaseModal.tsx
+++ frontend/src/components/base/BaseModal.tsx
@@ -23,13 +23,13 @@ export default function BaseModal({
   return createPortal(
     <div
       className="z-50 fixed bg-[rgba(0,0,0,0.7)] top-0 left-0 h-screen w-screen flex items-center justify-center"
-      onClick={onDismiss}
+      onMouseDown={onDismiss}
     >
       <div
         className={`flex flex-col p-5 rounded-xl bg-gray-900 text-gray-100 shadow-lg space-y-2 ${className} transition-all duration-150 ease-in ${
           visible ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-2"
         }`}
-        onClick={(e) => e.stopPropagation()}
+        onMouseDown={(e) => e.stopPropagation()}
       >
         {children}
       </div>
